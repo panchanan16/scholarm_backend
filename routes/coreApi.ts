ab018ae2ead05f59b7861175731889df7bfeff12
panchanan16 <dekapanchanan16@gmail.com>
2025-07-04 23:19:58 +0530

    $npm_config_msg

--- routes/coreApi.ts
+++ routes/coreApi.ts
@@ -1,9 +1,7 @@
 import { Router } from "express";
-import ArticleDetailControllers from "@/controllers/core/articledetailControllers";
 
 const coreRoute = Router();
 
-// Article Details -----
 
 
 
